b810cc0ae5ddbabf295cd660ff7b2dc81a628fbb
Brendan <150507223+holybreaded@users.noreply.github.com>
2024-02-03 10:18:48 +0800

    Update generator.py

--- generator.py
+++ generator.py
@@ -4,14 +4,13 @@ import string
 length = int(input("Enter Length: "))
 
 char = string.ascii_letters
-chars += string.digits
-chars += string.punctuation
+char += string.digits
+char += string.punctuation
 
-password = ''.join([random.choice(chars) for i in range(length)])
+password = ''.join([random.choice(char) for i in range(length)])
 
 for i in range (length):
-    next_character = random.choice(chars)
+    next_character = random.choice(char)
     password += next_character
 
 print("Your random password is:", password)
-
